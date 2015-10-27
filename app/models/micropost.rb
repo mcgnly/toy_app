6b64e939e29e64da323bead4c1cffa20f7c3cc5f
mcgnly <katie@mcgnly.com>
2015-10-27 15:53:24 +0100

    validate no empty fields in message, name, or ID

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,5 @@
 class Micropost < ActiveRecord::Base
-  validates :content, length: {maximum: 140}
+  validates :content, length: {maximum: 140},
+    presence : true #this means it can't be blank
   belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   has_many :microposts
+  validates FILL_IN, presence : true
+  validates FILL_IN, presence : true #makes sure both lines are filled in and throws an error if not
 end
